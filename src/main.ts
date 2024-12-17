21bd2abde2def984f1830469289c79d6d9c4582a
render-ydb <yangdaobo.render@gmail.com>
2024-12-17 21:39:08 +0800

    feat: add filter

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { logger } from './middleware/log.middleware';
-
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(logger);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
