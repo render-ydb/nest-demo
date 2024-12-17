fccf134f4087c36034a12ad7f5683632304074ea
render-ydb <yangdaobo.render@gmail.com>
2024-12-17 21:35:58 +0800

    feat: add middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { logger } from './middleware/log.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(logger);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
